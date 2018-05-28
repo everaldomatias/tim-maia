1b7891a7af2884bb88153ccfc25a07888e0de8a5
everaldomatias <matiaseveraldo@gmail.com>
2018-05-28 11:37:19 -0300

    Sprint na sessao nome, melhorando o tamanho e padding na home e paginas internas.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,29 +1,51 @@
 jQuery(document).ready(function() {
 
 	var parallax = jQuery( '.parallax-window' );
-	var body = jQuery( '.has-custom-logo' );
 	var height = window.innerHeight;
-
-	if ( jQuery( body ).length ) {
-
-		var logo = jQuery( 'img.logo' ).height();
-		var result = height * 0.5 - ( 160 * 0.5 );
-		var result_logo = height * 0.5 - ( logo * 0.5 + 56 );
-
-		jQuery( parallax ).css( 'height', height );
-		jQuery( parallax ).css( 'padding-top', result );
-		jQuery( '#section-nome' ).css( 'padding-top', result_logo );
-
-	} else if ( jQuery( parallax ).length ) {
-
-		var result_parallax = height * 0.5 - ( 160 * 0.5 );
-		
-		jQuery( parallax ).css( 'height', height );
-		jQuery( parallax ).css( 'padding-top', result_parallax );
-		//jQuery( parallax ).css( 'padding-bottom', result );
-		//jQuery( '#content' ).css( 'margin-top', height );
-		//jQuery( '#footer' ).css( 'margin-bottom', result );
+	var isHome = jQuery( 'body.home' );
+	var resizeTimer;
+	var heightFinal;
+	var size;
+	
+	if ( jQuery( isHome ).length ) {
+		size = jQuery( 'img.logo' ).height();
+		heightFinal = height;
+	} else {
+		size = jQuery( 'h1.entry-title' ).height();
+		heightFinal = height - 250;
 	}
+
+	// Resto da subtração da altura da tela menos a altura do título
+	var padding = heightFinal - size;
+	var paddingFinal = padding * 0.5;
+
+	jQuery( parallax ).css( 'height', heightFinal );
+	jQuery( parallax ).css( 'padding-bottom', paddingFinal + 10 );
+	jQuery( parallax ).css( 'padding-top', paddingFinal - 10 );
+
+	jQuery( window ).on( 'resize', function() {
+
+		clearTimeout( resizeTimer );
+		resizeTimer = setTimeout( function() {
+
+			if ( jQuery( isHome ).length ) {
+				size = jQuery( 'img.logo' ).height();
+				heightFinal = height;
+			} else {
+				size = jQuery( 'h1.entry-title' ).height();
+				heightFinal = height - 250;
+			}
+
+			// Resto da subtração da altura da tela menos a altura do título
+			var padding = heightFinal - size;
+			var paddingFinal = padding * 0.5;
+
+			jQuery( parallax ).css( 'height', heightFinal );
+			jQuery( parallax ).css( 'padding-bottom', paddingFinal + 10 );
+			jQuery( parallax ).css( 'padding-top', paddingFinal - 10 );
+
+		}, 200);
+	});
 
 	jQuery(function() {
 	  jQuery('a[href*=#]:not([href=#])').click(function() {
